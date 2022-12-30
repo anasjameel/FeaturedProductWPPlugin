accfaf192a84fc723ae81c22d0769f7a19514031
anasjameel <anasjameel14@gmail.com>
2022-12-29 19:28:44 -0500

    Added return token

--- featured-scroller.php
+++ featured-scroller.php
@@ -3,26 +3,29 @@
 
 
 function featured_scroller_atts($atts) {
- 	$default = array(
-        'cat-slug' => null
+  $default = array(
+        'cat-slug' => ""
     );
     $a = shortcode_atts($default, $atts);
     $slug = $a['cat-slug'];
     $product_args = array(
         'post_status' => 'publish',
-        'limit' => -1,
+        'limit' => 10,
         'category' => $slug
     );
-    $products = wc_get_products($product_args); ?>
-    <div class="owl-carousel" id="featured-product-scroller">
-    <?php
-	$index = 1;
+    $products = wc_get_products($product_args); 
+    $index = 1;
+    $sliderItems = "";
     foreach ($products as $product) :
-    	
-    	$product_id = $product->get_id();
-	    $product_type = $product->get_type();
-	    $product_title = $product->get_title();
-	    $product_permalink = $product->get_permalink();
+      
+      $product_id = $product->get_id();
+      $product_type = $product->get_type();
+      $product_title = $product->get_title();
+      $product_permalink = $product->get_permalink();
+      $imgUrl = wp_get_attachment_thumb_url($product->get_image_id());
+            if (empty($imgUrl)) {
+              $imgUrl = 'https://gearsupply.com/wp-content/uploads/2021/01/product_placeholder-square.png';
+            }
         $utm = '?utm_source=gearsupply&utm_medium=blog&utm_campaign=Blog+CRO';
         $link = $product_permalink.$utm;
         $newDesc1 = strip_tags($product->get_description());
@@ -38,7 +41,7 @@ function featured_scroller_atts($atts) {
               {
                 $newDesc1 = '<p class=fs-desc>' .$newDesc1. '</p>';
            }
-	    $price1 = $product->get_price_html();
+      $price1 = $product->get_price_html();
             if(empty($price1))
             {
                $price1 = 'Check Availability';
@@ -46,35 +49,26 @@ function featured_scroller_atts($atts) {
             else {
               $price1 = $price1.' on Gearsupply';
             }     
-		$product_main_image_id = $product->get_image_id();
-		if ($product_main_image_id) :
-	        $product_main_image = wp_get_attachment_image_src($product_main_image_id, 'full');
-	        $product_main_image_alt = get_post_meta($attachment_id, '_wp_attachment_image_alt', TRUE);
-	    endif;
-?>
-    <div id="slider-item-<?php echo $index;?>" class="item">
-        <a href="<?php echo $link ?>">
-            <?php if ($product_main_image) : ?>
-                <img src="<?php echo $product_main_image[0]; ?>" alt="<?php echo $product_main_image_alt ?>">
-            <?php endif; ?>
-        </a>
-        <a href="<?php echo $link ?>">
-        	<h2 class="scroller-product-title"><?php echo $product_title ?></h2>
-        </a>
-          <div class="fs-button fs-btn-scroller">
-                  <a class="check-price" href="<?php echo $link ?>"><?php echo $price1 ?></a>
-              </div>
- 
-            <?php  echo $newDesc1 ?>
-            <!-- display more product infos -->
-        
-    </div>
+    $product_main_image_id = $product->get_image_id();
+    if ($product_main_image_id) :
+          $product_main_image = wp_get_attachment_image_src($product_main_image_id, 'full');
+          $product_main_image_alt = get_post_meta($attachment_id, '_wp_attachment_image_alt', TRUE);
+      endif;
 
-<?php $index++;	
-	endforeach; 
-?>
-</div>
-<?php
+  $sliderItems = $sliderItems."<div class='slider-item-".$index." item'>
+        <a href=".$link."><img src=".$imgUrl." alt=".$product_main_image_alt."></a>
+        <a href=".$link."><h3 class='scroller-product-title'>".$product_title."</h3></a>
+        <div class='fs-button fs-btn-scroller'><a class='check-price' href=".$link.">".$price1."</a></div>".$newDesc1."
+        </div>";
+
+ 
+    $index++; 
+  endforeach; 
+
+$output = "<div class='owl-carousel' id='featured-product-scroller'>".$sliderItems."</div>";
+
+
+return $output;
    
 }
 
